85357078801b6f355125bccadce6ec6cc96bb872
roeyHonig <roeyhonig94@gmail.com>
2018-09-03 12:45:51 +0300

    log in into Proggio and create a new Proj

--- src/student/roey/honig/Main.java
+++ src/student/roey/honig/Main.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,16 +20,24 @@ public class Main {
          By inputPasswordTextFieldId = new By.ById("input-password");
         By signInBtnId = new By.ById("button-login");
 
+        By newProjectElementTitle = new By.ByClassName("new");
+
         WebElement emailInput = obj.findElement(inputTextFieldId);
         emailInput.clear();
         emailInput.sendKeys("roeyhonig94@gmail.com");
 
         WebElement passwordInput = obj.findElement(inputPasswordTextFieldId);
         passwordInput.clear();
-        passwordInput.sendKeys("14ZDDo01");
+        passwordInput.sendKeys("12345678");
 
         WebElement signInBtn = obj.findElement(signInBtnId);
         signInBtn.click();
+
+        // make sure page loads
+        obj.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement newProjBtn = obj.findElement(newProjectElementTitle);
+        newProjBtn.click();
 
 
     }
