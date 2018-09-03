fb2a30d6713d3a94519a00d4a1556d5bef943de8
roeyHonig <roeyhonig94@gmail.com>
2018-09-03 11:43:48 +0300

    extracting some data from the url

--- src/student/roey/honig/Main.java
+++ src/student/roey/honig/Main.java
@@ -1,5 +1,6 @@
 package student.roey.honig;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +12,13 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:/roey/chromeDriverForSolenium/chromedriver.exe");
         WebDriver obj = new ChromeDriver();
         obj.get("https://responsive-cv.firebaseapp.com/");
+
+        By nameHeaderId = new By.ById("name");
+        //By educationHeaderId = new By.ById("educationH1Id");
+
+        System.out.println("The person this CV beloings to is: " + obj.findElement(nameHeaderId).getText());
+        //System.out.println(obj.findElement(educationHeaderId).getText());
+
     }
 }
 
